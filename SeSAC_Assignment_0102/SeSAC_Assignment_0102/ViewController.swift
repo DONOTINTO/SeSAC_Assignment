2a659f9fc86741eb2712debf4c65f6d832a66154
DONOTINTO <goods99j@gmail.com>
2024-01-03 17:18:20 +0900

    Add_SeSAC_Assignment_0102

---
+++ SeSAC_Assignment_0102/SeSAC_Assignment_0102/ViewController.swift
@@ -0,0 +1,80 @@
+//
+//  ViewController.swift
+//  SeSAC01.02_SB_HW
+//
+//  Created by 이중엽 on 1/2/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    @IBOutlet var imageStackView: [UIStackView]!
+    @IBOutlet var labelStackView: [UIStackView]!
+    @IBOutlet var imageViews: [UIImageView]!
+    @IBOutlet var labels: [UILabel]!
+    var labelTitle = ["행복해", "사랑해", "좋아해", "당황해", "속상해", "우울해", "심심해", "행복해", "눈물나"]
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        setUI()
+        setTapGesture()
+    }
+    
+    @objc func imageViewClicked(_ sender: UITapGestureRecognizer) {
+        guard let image = sender.view as? UIImageView else { return }
+        let idx = image.tag
+        
+        //클릭마다 Label Tag에 1씩 추가
+        labels[idx].tag += 1
+        
+        labels[idx].text = "\(labelTitle[idx]) \(labels[idx].tag)"
+        
+        UIView.animate(withDuration: 0.03, delay: 0, options: .autoreverse , animations: {
+            image.transform = CGAffineTransform(scaleX: 1.05, y: 1.05)
+        }, completion: { _ in
+            image.transform = CGAffineTransform(scaleX: 1, y: 1)
+        })
+        
+        if labels[idx].tag == 10 {
+            let alert = UIAlertController(title: "히든미션", message: "\(labelTitle[idx]) 10회 클릭하기", preferredStyle: .alert)
+            let okAction = UIAlertAction(title: "달성", style: .default)
+            alert.addAction(okAction)
+            present(alert, animated: true)
+        }
+    }
+    
+    func setTapGesture() {
+        imageViews.forEach {
+            let tapGesture = UITapGestureRecognizer(target: self, action: #selector(imageViewClicked(_:)))
+            $0.isUserInteractionEnabled = true
+            $0.addGestureRecognizer(tapGesture)
+        }
+    }
+
+    func setUI() {
+        imageStackView.forEach {
+            $0.distribution = .fillEqually
+            $0.spacing = 10
+        }
+        
+        labelStackView.forEach {
+            $0.distribution = .fillEqually
+            $0.spacing = 10
+        }
+        
+        for idx in 0 ..< imageViews.count {
+            imageViews[idx].image = UIImage(named: "slime\(idx + 1)")
+            //image View Tag에 인덱스 넘버 저장하여 사용
+            imageViews[idx].tag = idx
+        }
+        
+        for idx in 0 ..< labels.count {
+            labels[idx].text = labelTitle[idx]
+            labels[idx].textAlignment = .center
+            labels[idx].font = .boldSystemFont(ofSize: 12)
+            labels[idx].textColor = .black
+        }
+    }
+}
+
