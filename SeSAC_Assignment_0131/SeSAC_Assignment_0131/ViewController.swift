ac2ffb01ce08bd0b3548bc5ff034c6dc20770525
DONOTINTO <goods99j@gmail.com>
2024-01-31 21:01:28 +0900

    Add - 0131 Assignment Project

---
+++ SeSAC_Assignment_0131/SeSAC_Assignment_0131/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SeSAC_Assignment_0131
+//
+//  Created by 이중엽 on 1/31/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
