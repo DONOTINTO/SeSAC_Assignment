e053fe9015f60edaf2fbb14a97092ee75c3aad56
DONOTINTO <goods99j@gmail.com>
2024-02-05 03:51:29 +0900

    Create UIView++.swift

---
+++ SeSAC_Assignment_0131/SeSAC_Assignment_0131/Extension/UIView++.swift
@@ -0,0 +1,14 @@
+//
+//  UIView++.swift
+//  SeSAC_Assignment_0131
+//
+//  Created by 이중엽 on 2/5/24.
+//
+
+import UIKit
+
+extension UIView {
+    static var identifier: String {
+        return String(describing: self)
+    }
+}
